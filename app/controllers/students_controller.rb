24d778138ed797aa33347f512cd8fcba382da70f
rtetelbaum <rtetelbaum@mailbox.org>
2020-10-07 00:03:17 -0400

    lab done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,12 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
+	def create
+		student = Student.new
+		student.first_name = params[:first_name]
+		student.last_name = params[:last_name]
+		student.save
+		redirect_to student
   end
 
 end
